272378d33e6c3ff6450f4aff1295430dae2378b0
ABDELOUAHED ABBAD <moabbad22@gmail.com>
2025-02-27 15:22:33 +0000

    tp3 is done

--- TP-3/obj/Debug/net9.0/TP-3.AssemblyInfo.cs
+++ TP-3/obj/Debug/net9.0/TP-3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c1fb873e0cbba02af3e25c70f500835b07bf5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
